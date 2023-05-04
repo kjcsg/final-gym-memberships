6806cba538e12acf9e87f5f6568f8b543a07aa9c
Kyle Comsa (Laptop) <kjcsg17@gmail.com>
2023-05-04 10:24:46 -0700

    Updated database

--- src/Database.java
+++ src/Database.java
@@ -4,20 +4,26 @@ public class Database {
     HashMap<Integer, Member> db = new HashMap<>(); //Setup database
 
     public void add(Member member){ //Add member to database
-        int id = member.id;
-        db.put(id, member);
+        db.put(member.id, member);
     }
 
     public Member find(int id){ //Find member from id number and return member object
-        Member member = db.get(id);
-        return member;
+        return db.get(id);
     }
 
-    private Array qs(){
-
+    private String[] qs(){
+        String[] names = new String[db.size()];
+        for(int i=0; i<db.size(); i++){
+            names[i]=db.get(i).lname;
+        }
+        return names;
     }
 
-    public String NamesString(){
-        
+    public String NamesString(String[] arr){ //Convert array of names to comma separated names list
+        String out = arr[0];
+        for(String name : arr){
+            out+=", "+name;
+        }
+        return out;
     }
 }
